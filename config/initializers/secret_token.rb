688fc0ea65627728e2f3262d5d25f4741e9d72f9
npcomp <npcomp@playground.(none)>
2012-12-17 12:01:43 -0500

    Initial checking of peds rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peds::Application.config.secret_token = 'c0bd88d74784e1be49213bb25b0c92af1fe628fca221771c92a46076074f37d84f659ba46b91e61187ef2e59bc45e933dcb411f7e3e0a54246624b3d857dbada'
